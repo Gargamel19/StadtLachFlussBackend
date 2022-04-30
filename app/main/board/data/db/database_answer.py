9194f6422ebfcaab1478a53d1bcbbf1ded68d527
Ferdinand <trendelenburger19.04@googlemail.com>
2022-05-01 01:22:09 +0200

    get all player added

--- app/main/board/data/db/database_answer.py
+++ app/main/board/data/db/database_answer.py
@@ -17,7 +17,7 @@ class DatabaseAnswer:
                   "CATEGORY_ID INTEGER NOT NULL," \
                   "PLAYER_ID INTEGER NOT NULL," \
                   "TEXT TEXT NOT NULL," \
-                  "ANSWER_VISIBLE TEXT DEFAULT 'False'" \
+                  "ANSWER_VISIBLE TEXT" \
                   ")"
             DataBase.make_no_response_query(sql, DatabaseAnswer.path)
         except OperationalError:
@@ -38,10 +38,7 @@ class DatabaseAnswer:
         answer = DataBase.make_multi_response_query(query, DatabaseAnswer.path)
         for cat in answer:
             if cat:
-                if bool(cat[4]):
-                    answers.append(Answer(int(cat[0]), int(cat[1]), int(cat[2]), cat[3], "True"))
-                else:
-                    answers.append(Answer(int(cat[0]), int(cat[1]), int(cat[2]), cat[3], "False"))
+                answers.append(Answer(int(cat[0]), int(cat[1]), int(cat[2]), cat[3], cat[4]))
         return answers
 
     @staticmethod
@@ -51,10 +48,8 @@ class DatabaseAnswer:
         answer = DataBase.make_multi_response_query(query, DatabaseAnswer.path)
         for cat in answer:
             if cat:
-                if bool(cat[4]):
-                    answers.append(Answer(int(cat[0]), int(cat[1]), int(cat[2]), cat[3], "True"))
-                else:
-                    answers.append(Answer(int(cat[0]), int(cat[1]), int(cat[2]), cat[3], "False"))
+                answers.append(Answer(int(cat[0]), int(cat[1]), int(cat[2]), cat[3], cat[4]))
+
         return answers
 
     @staticmethod
@@ -64,11 +59,7 @@ class DatabaseAnswer:
         if answer and len(answer) == 1:
             player_obj = answer[0]
             if player_obj:
-                if bool(player_obj[4]):
-                    answer = Answer(int(player_obj[0]), int(player_obj[1]), int(player_obj[2]), player_obj[3], "True")
-                else:
-                    answer = Answer(int(player_obj[0]), int(player_obj[1]), int(player_obj[2]), player_obj[3], "False")
-                return answer
+                return Answer(int(player_obj[0]), int(player_obj[1]), player_obj[2], player_obj[3])
             else:
                 return player_obj
         else:
