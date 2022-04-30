86d0307cc9aeb840bba100e9b7df4576fd645555
Ferdinand <trendelenburger19.04@googlemail.com>
2022-04-30 22:02:00 +0200

    added game id in Category Object

--- app/main/board/data/models/Category.py
+++ app/main/board/data/models/Category.py
@@ -1,12 +1,14 @@
 class Category:
     cat_id = 0
+    game_id = 0
     name = ""
     proposal = ""
 
-    def __init__(self, cat_id, name, proposal):
+    def __init__(self, cat_id, game_id, name, proposal):
         self.cat_id = cat_id
+        self.game_id = game_id
         self.name = name
         self.proposal = proposal
 
     def __repr__(self):
-        return str({"cat_id": self.cat_id, "name": self.name, "proposal": self.proposal}).replace("'", "\"")
+        return str({"cat_id": self.cat_id, "name": self.name, "game_id": self.game_id, "proposal": self.proposal}).replace("'", "\"")
